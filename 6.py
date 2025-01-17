f7e8953e0051304aed5a4708e73c0e73c1642a34
Kudaibergen <k_tukubayev@kbtu.kz>
2025-01-17 17:34:24 +0500

    6.py: casting

---
+++ 6.py
@@ -0,0 +1,14 @@
+#CASTING 
+
+x = int(input("Enter int number pls: "))
+print("Casting int to float:", x, "=", float(x))
+print("Casting int to str:", x, "=", str(x))
+
+y = float(input("Enter float number pls: "))
+print("Casting float to int:", y, "=", int(y))
+print("Casting float to str:", y, "=", str(y))
+
+z = str(input("Enter number pls:"))
+print(type(z))
+print("From string to int:", int(z))
+print("From string to float:", float(z))
