2d4f038d29ce7a5023f0404b7ee080b4b66043da
Kudaibergen <k_tukubayev@kbtu.kz>
2025-01-22 10:19:08 +0500

    10.py: change list items and add list items

---
+++ 10.py
@@ -0,0 +1,83 @@
+print("""thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)""")
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)""")
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)""")
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+""")
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)""")
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)""")
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)""")
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)""")
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)""")
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
