bee272e41e0bbf405bd89393d2289095bc318aa4
Kudaibergen <k_tukubayev@kbtu.kz>
2025-01-22 10:13:42 +0500

    9.py: py lists and items

---
+++ 9.py
@@ -0,0 +1,59 @@
+print("""thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)""")
+
+print()
+
+list1 = ["abc", 34, True, 40, "male"]
+print(list1)
+print("List can involve different types of data")
+
+print()
+
+print("""List is a collection which is ordered and changeable. Allows duplicate members.
+Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+Dictionary is a collection which is ordered** and changeable. No duplicate members.""")
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])""")
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])""")
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])""")
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])""")
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])""")
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+print()
+
+print("""thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")""")
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
